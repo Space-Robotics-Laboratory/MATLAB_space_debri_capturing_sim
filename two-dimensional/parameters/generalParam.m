84b41c03234c6ccd2534734f05c54a8cedace3dd
Akiyoshi Uchida <uchida.akiyoshi.s3@dc.tohoku.ac.jp>
2023-08-21 11:57:14 +0900

    DIRECT capture

--- two-dimensional/parameters/generalParam.m
+++ two-dimensional/parameters/generalParam.m
@@ -7,7 +7,7 @@ gParam.dataSaveName = 'myFile';
 
 %% Simulation Time Setting
 gParam.minusTime = 0; % minus value like -1
-gParam.endTime   = 45;
+gParam.endTime   = 5;
 gParam.divTime   = .001;
 
 % Simulation Break Setting
