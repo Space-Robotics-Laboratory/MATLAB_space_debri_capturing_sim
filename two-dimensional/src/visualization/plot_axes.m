f2d2c0d22b04593d4d60a6b4232023dec24596a9
AkiyoshiUchida <uchida.akiyoshi.s3@dc.tohoku.ac.jp>
2024-04-13 16:06:08 +0900

    change arrow head size

--- two-dimensional/src/visualization/plot_axes.m
+++ two-dimensional/src/visualization/plot_axes.m
@@ -7,7 +7,9 @@ function plot_axes(ori_mat, pos)
 size = 0.06; % 3*4
 texpos = pos + ori_mat*size;
 
-quiver(repmat(pos(1), 1, 2), repmat(pos(2), 1, 2), ori_mat(1,1:2), ori_mat(2,1:2), size, LineWidth=2.2, Color="black")
+quiver(repmat(pos(1), 1, 2), repmat(pos(2), 1, 2), ori_mat(1,1:2), ori_mat(2,1:2), ...
+    size, LineWidth=2.2, Color="black", MaxHeadSize=2)
+
 % text(texpos(1,1), texpos(2,1), "x", "FontSize",20)
 % text(texpos(1,2), texpos(2,2), "y", "FontSize",20)
 
