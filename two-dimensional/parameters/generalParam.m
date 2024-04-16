ecb89ae1b932674693cacf534150dfe33f0b77ed
AkiyoshiUchida <uchida.akiyoshi.s3@dc.tohoku.ac.jp>
2024-04-16 22:41:02 +0900

    export direct caging case with long duration after the capture

--- two-dimensional/parameters/generalParam.m
+++ two-dimensional/parameters/generalParam.m
@@ -7,19 +7,19 @@ gParam.dataSaveName = 'myFile';
 
 %% Simulation Time Setting
 gParam.minusTime = 0; % minus value like -1
-gParam.endTime   = 100;
+gParam.endTime   = 25;
 gParam.divTime   = .001;
 
 % Simulation Break Setting
-gParam.breakTimeDuration = 6;
+gParam.breakTimeDuration = 25;
 
 %% Animation Time Setting
-gParam.visualizeAnimation = true;
+gParam.visualizeAnimation = false;
 gParam.saveData = true;
 gParam.makeAnimation = true;
 gParam.makeGraph = true;
 gParam.anime_frameRate = 10;    % fps
-gParam.snapShot_frameRate = 1; % fps
+gParam.snapShot_frameRate = 10; % fps
 
 
 end
