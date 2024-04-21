d094614c7bde8426ac352bd398a8e43412c76034
akiyoshi <uchida.akiyoshi.s3@dc.tohoku.ac.jp>
2024-04-21 16:52:31 +0900

    add joint position plot

--- two-dimensional/src/analysis/make_graph.m
+++ two-dimensional/src/analysis/make_graph.m
@@ -202,8 +202,10 @@ saveas(figure(figureNumber), [paths.figfile, '/', pngName]);    % png保存
 
 %%% make robot base attitude graph
 %%% make velocity-based manipulability ellipsoid are size graph
+%%% make joint position graph
 baseQ = datStruct.roboQ0(:, 3);
 velManip = datStruct.velocityManipulability;
+jointsPosition = datStruct.jointAng';
 
 figureNumber = figureNumber+1;     % 図番号設定
 figure(figureNumber);   % 図定義
@@ -212,15 +214,18 @@ yyaxis left
 plot(time, baseQ, "LineWidth", lineWidth)
 box on
 hold on
+plot(time, jointsPosition([1:3,5:7], :), "LineWidth", lineWidth)
 yyaxis right
 plot(time, velManip(:,1), "LineWidth", lineWidth)
 plot(time, velManip(:,2), "LineWidth", lineWidth)
-legend('Base attitude', 'Left-arm', 'Right-arm')
+legend('Base attitude', ...
+  'Joint 1', 'Joint 2', 'Joint 3', 'Joint 4', 'Joint 5', 'Joint 6', ...
+  'Left-arm', 'Right-arm')
 plotSequenceStateArea(datStruct)
 set(gca, 'FontSize', fontSize);  % 軸目盛りのフォントサイズを設定
 yyaxis left
 if(showTitle)
-    title("Robot Base Attitude")
+    title("Robot Stability")
 end
 ylabel("Attitude [rad]")
 xlabel("Time [s]")
@@ -229,8 +234,8 @@ ylabel("Measure of manipulability [-]")
 xlim([time(1), time(time_length)])
 box off
 
-figName = 'baseOri.fig';                                  % fig名定義
-pngName = 'baseOri.png';                                  % png名定義
+figName = 'stability.fig';                                  % fig名定義
+pngName = 'stability.png';                                  % png名定義
 saveas(figure(figureNumber), [paths.figfile, '/', figName]);    % fig保存
 saveas(figure(figureNumber), [paths.figfile, '/', pngName]);    % png保存
 
