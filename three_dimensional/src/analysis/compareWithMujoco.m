5f3b4a081458761bd69c96a8e71863fa52aba716
akiyoshi <uchida.akiyoshi.s3@dc.tohoku.ac.jp>
2024-05-14 23:16:03 +0900

    ad graph

--- three_dimensional/src/analysis/compareWithMujoco.m
+++ three_dimensional/src/analysis/compareWithMujoco.m
@@ -1,7 +1,16 @@
 mat_csv = "\dat\2024-0509\2024-0509-225953\testRk-dat\savedDat.csv";
 mat_tabel = readtable(mat_csv);
 mat_ang_vel = [mat_tabel.targetWb_1, mat_tabel.targetWb_2, mat_tabel.targetWb_3];
+mat_time = mat_tabel.time;
 
 mj_csv = "../../mujoco_dar_simulation/tutorial/case0/ang_vel.csv";
 mj_table = readtable(mj_csv);
-mj_ang_vel = [mj_table.Var1, mj_table.Var2, mj_table.Var3]
+mj_time = mj_table.Var1;
+mj_ang_vel = [ mj_table.Var2, mj_table.Var3, mj_table.Var4];
+
+
+diff_ang_vel = mat_ang_vel(2:end,:) - mj_ang_vel(1:end-1,:);
+
+plot(diff_ang_vel)
+legend("x", "y", "z")
+title("Difference between mujoco and spacedyn (dt=0.001)")
